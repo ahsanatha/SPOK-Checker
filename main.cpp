e16e72c6d4300ec1060e12d4f3bd4c38f76c5902
ahsanatha <ahsan.athallah@gmail.com>
2018-12-03 13:55:26 +0700

    up up

--- main.cpp
+++ main.cpp
@@ -9,13 +9,53 @@ bool S_Check(string word){
     string kamus[5] = {"saya","kamu","dia","mereka","kami"};
     //membuat "po" sebagai pointer.
     int po = 0;
-    //
+    //cek apakah word ada di kamus.
     while(!cek && (po <= 5)){
         cek = kamus[po] == word;
         po++;
     }
     return cek;
 }
+bool P_Check(string word){
+    bool cek = false;
+    //kamus_subject should only contain lower case words.
+    string kamus[5] = {"membeli","belanja","makan","berlari","pergi"};
+    //membuat "po" sebagai pointer.
+    int po = 0;
+    //cek apakah word ada di kamus.
+    while(!cek && (po <= 5)){
+        cek = kamus[po] == word;
+        po++;
+    }
+    return cek;
+}
+bool O_Check(string word){
+    bool cek = false;
+    //kamus_subject should only contain lower case words.
+    string kamus[5] = {"membeli","belanja","makan","berlari","pergi"};//belum diganti
+    //membuat "po" sebagai pointer.
+    int po = 0;
+    //cek apakah word ada di kamus.
+    while(!cek && (po <= 5)){
+        cek = kamus[po] == word;
+        po++;
+    }
+    return cek;
+}
+bool K_Check(string word){
+    bool cek = false;
+    //kamus_subject should only contain lower case words.
+    string kamus[5] = {"membeli","belanja","makan","berlari","pergi"};//belum di ganti.
+    //membuat "po" sebagai pointer.
+    int po = 0;
+    //cek apakah word ada di kamus.
+    while(!cek && (po <= 5)){
+        cek = kamus[po] == word;
+        po++;
+    }
+    return cek;
+}
+
 
 int main()
 {
