2403276c40cf69390291ccea0a02aefd541b66fc
ahsanatha <ahsan.athallah@gmail.com>
2018-12-03 10:24:22 +0700

    add S and P checker

--- main.cpp
+++ main.cpp
@@ -1,9 +1,32 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
+//a subject checker that will return true or false.
+bool S_Check(string word){
+    bool cek = false;
+    //kamus_subject should only contain lower case words.
+    string kamus[5] = {"saya","kamu","dia","mereka","kami"};
+    //membuat "po" sebagai pointer.
+    int po = 0;
+    //
+    while(!cek && (po <= 5)){
+        cek = kamus[po] == word;
+        po++;
+    }
+    return cek;
+}
+
 int main()
 {
+    string word;
     cout << "Hello world!" << endl;
+    do{
+        cin >> word;
+        if(S_Check(word))
+            cout << "terima" << endl;
+        else
+            cout << "tolak" << endl;
+    }while(word != "exit");
     return 0;
 }
